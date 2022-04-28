6d4e6ee804ff64e8f01bbcee2ba5b76ad51637fe
Remilio11 <emimm96@gmail.com>
2022-04-28 15:12:22 -0500

    proycicam

---
+++ backEnd/performance/aermod/aermod.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+let promesaEscrituraArchivo = new Promise((resolve, reject) => {
+    fs.writeFile('aermod.inp', 'JOB \r\n    MESSAGES MET2119246_AERMET_2019-2020.ER3 \r\n    REPORT   MET2119246_AERMET_2019-2020.OU3\r\n \r\nMETPREP\r\n    DATA     MET2119246_AERMET_2019-2020.DAT.MER\r\n    XDATES   2019/01/01 2020/12/31', 
+    (error) => {
+        if(error){
+            reject(error);
+        } else {
+            resolve();
+        }
+    });
+});
+
+promesaEscrituraArchivo
+.then(() => {
+    console.log('La escritura del archivo ha sido satisfactoria.');
+})
+.catch((error) => {
+    console.log('Ha ocurrido un error:', error);
+});
---
+++ backEnd/server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const conectDB = require('./config/db');
+
+const bodyParser = require('body-parser');
+const multipart = require('connect-multiparty');
+const cors = require('cors');
+const multer = require('multer');
+
+const PORT = 3000;
+
+//Guardar el archivo que viene desde el front
+const storage = multer.diskStorage({
+    filename: function(res, file, cb){
+        //const ext = file.originalname.split('.').pop(); //Aqui se quedo la extensión .pfl, .inp, etc
+        const filename = file.originalname; //nombre del archivo
+        cb(null, `${filename}`)  //nombre con extencion aermet.inp
+    },
+    destination: function(res, file, cb){
+        cb(null, `./performance/aermet`)
+    }
+})
+
+const upload = multer({storage})
+
+//creamos el servidor
+const app = express();
+app.use(cors());
+
+//Conectarnos a la base
+conectDB();
+
+//Configuración Middleware
+app.use(express.json());
+
+app.use('/api/registro', require('./routes/persona'));
+
+//Endpoint to Upload files
+app.post('/upload', upload.single('myFile') ,(req, res) => {
+    res.send({
+        data: 'Fichero ok'
+    })
+});
+
+//Verificación del funcionamiento del Servidor
+//Respuesta en el navegador al servidor levantado
+app.get('/', (req, res) =>{
+    res.send('El servidor backEnd en funcionamiento');
+});
+
+app.listen(PORT, () => {
+    console.log(`El servidor corre perfectamente por el puerto: ${PORT}`);
+})
